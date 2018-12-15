86ddf5043c32a64eb1d724f29ac83222436f5b09
Adam Láníček <xlanic04@fit.vutbr.cz>
2018-12-15 17:05:21 +0100

    Alembic clean-up

--- webapp/alembic/versions/ee542665fc4c_new_primary_key_in_vozidlo_table_id_.py
+++ webapp/alembic/versions/ee542665fc4c_new_primary_key_in_vozidlo_table_id_.py
@@ -17,18 +17,12 @@ depends_on = None
 
 
 def upgrade():
-    op.execute("alter table dokument drop foreign key dokument_ibfk_1;")
     op.drop_constraint('PRIMARY', 'vozidlo', type_='primary')
     op.execute("DELETE FROM vozidlo;")
     op.execute("ALTER TABLE vozidlo ADD id_voz int NOT NULL PRIMARY KEY AUTO_INCREMENT;")
-    op.alter_column('dokument', 'spz', type_=sa.Integer, existing_type=sa.String(length=10), nullable=False, new_column_name='id_voz')
-    op.create_foreign_key('fk_vozidlo_dokument', 'dokument', 'vozidlo', ['id_voz'], ['id_voz'])
-
 
 def downgrade():
-    op.drop_constraint('fk_vozidlo_dokument', 'dokument', type_='foreignkey')
+    op.execute("DELETE FROM vozidlo;")
     op.drop_column('vozidlo', 'id_voz')
-    op.alter_column('dokument', 'id_voz', nullable=False, existing_type=sa.Integer, type_=sa.String(length=10), new_column_name='spz')
     op.create_primary_key('PRIMARY', 'vozidlo', ['spz'])
-    op.create_foreign_key('dokument_ibfk_1', 'dokument', 'vozidlo', ['spz'], ['spz'])
 
